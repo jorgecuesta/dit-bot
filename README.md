8de89eb6ab93ab4c24888a901b6f464cba2bb1b1
Jorge Cuesta <jckpo2@gmail.com>
2018-04-12 13:54:58 -0300

    Wip to improve code quality

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# dit-bot
-Destiny Item Tracker Bot
+### Destiny 2 Item Tracker Bot
+
+Little bot for [Discord](https://) that used [Bungie](https://bungie.net) API to ask search terms.
+
+#### Deploy:
+
+1. `npm i` or `yarn`
+2. Create your own `.env` file following `.env.sample` file.
+  * NODE_ENV= Environment (development|production) Default: development
+  * TRIGGER= Command trigger like ! Default: !
+  * BUNGIE_API_KEY= Bungie X-API-Key
+  * DISCORD_BOT_TOKEN= Discord Bot Token
+  * LOCALE= Language that bot will use to search and publish messages (Spanish|SpanishMexican|English)
+
+#### Commands:
+
+* [trigger]buscar [option] [search terms]
+* [trigger]ayuda
+
+
+#### TODOS:
+* I18N commands and options.
--- worker.js
+++ worker.js
@@ -10,42 +10,27 @@ const TypeDefinition = require('the-traveler/build/enums').TypeDefinition;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-  colorize: true,
-});
-logger.level = 'debug';
+logger.add(logger.transports.Console, {colorize: true});
+logger.level = process.env.NODE_ENV !== 'production' ? 'debug' : 'error';
 
-const token = 'NDMzNzk1OTM4NzA0Njg3MTA0.DbBMzg.tQxlx0vx6wW7OIGqhySf7JHU6YM';
+if (!process.env.BUNGIE_API_KEY || !process.env.DISCORD_BOT_TOKEN) {
+  throw new Error('missing environment values.');
+}
+
+const TRIGGER = process.env.TRIGGER || '!';
+
+const locale = process.env.LOCALE || 'Spanish';
+
+const token = process.env.DISCORD_BOT_TOKEN;
+
 // Initialize Discord Bot
 const bot = new Discord.Client();
 
 const traveler = new Traveler({
-  apikey: '1ce223b722b146f89a7446a9cc6ebb94',
-  userAgent: 'dit-bot', // used to identify your request to the API
-  debug: true,
+  apikey: process.env.BUNGIE_API_KEY,
+  userAgent: 'd2it-bot', // used to identify your request to the API
+  debug: process.env.NODE_ENV !== 'production',
 });
-
-const bungiebase = 'https://bungie.net';
-const ditbase = 'https://db.destinytracker.com/d2/es/items/';
-
-let locales = {
-  'Spanish': 'es',
-};
-
-rp({
-  method: 'GET',
-  uri: 'https://www.bungie.net/Platform/GetAvailableLocales',
-  headers: {
-    'X-API-Key': '1ce223b722b146f89a7446a9cc6ebb94',
-  },
-  json: true,
-}).then(function(response) {
-  locales = response.Response;
-}).catch(function(error) {
-  logger.error(error, 'unable to read locales');
-});
-
-logger.info('starting bot');
 
 const itemTypeMap = {
   'armaduras': 2,
@@ -68,6 +53,10 @@ const itemTypeMap = {
 
 const commands = ['buscar', 'ayuda'];
 
+let locales = {
+  'Spanish': 'es',
+};
+
 const commandNotFound = function(message, requiredHelp = false) {
   const embed = new Discord.RichEmbed()
       .setTitle('Ayuda')
@@ -80,112 +69,131 @@ const commandNotFound = function(message, requiredHelp = false) {
   message.channel.send({embed});
 };
 
-bot.on('ready', function() {
-  logger.info('I am ready!');
-});
+logger.info('starting bot');
 
-bot.on('disconnect', function(errMsg, code) {
-  logger.info(arguments);
-  logger.error(errMsg, code);
-});
+const onLocaleResponse = function(response) {
+  locales = response.Response;
 
-bot.on('message', async function(message) {
-  if (message.author.bot) return;
-  if (message.content.indexOf('!') !== 0) return;
-  logger.debug('Incoming message');
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
+  const bungiebase = 'https://bungie.net';
+  const ditbase = `https://db.destinytracker.com/d2/${locales[locale]}/items/`;
 
-  // This is the best way to define args. Trust me.
-  const args = message.content.slice(1).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
+  bot.on('ready', function() {
+    logger.info('I am ready!');
+  });
 
-  switch (cmd) {
-    case 'buscar':
-      const type = itemTypeMap[args.shift().toLowerCase()];
-      const term = args.join(' ');
+  bot.on('disconnect', function(error) {
+    logger.error(error);
+    process.exit(1);
+  });
 
-      if (!type || term.length === 0) {
-        const embed = new Discord.RichEmbed()
-            .setTitle('Ayuda')
-            .setColor(0x00AE86)
-            .setDescription('Para buscar debes poner algo como esto: !buscar [opciones] [busqueda]')
-            .setTimestamp()
-            .addField('Opciones', _.keys(itemTypeMap).join(', '), true);
+  bot.on('message', async function(message) {
+    if (message.author.bot) return;
+    if (message.content.indexOf(TRIGGER) !== 0) return;
 
-        message.channel.send({embed});
-        return;
-      }
+    logger.debug('Incoming message');
 
-      logger.debug('term of search', term);
+    const args = message.content.slice(TRIGGER.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
 
-      const response = await traveler.searchDestinyEntities(
-          querystring.escape(term),
-          TypeDefinition.DestinyInventoryItemDefinition,
-          {
-            groups: [DestinyStatsGroupType.Weapons, DestinyStatsGroupType.UniqueWeapon],
-            lc: locales.SpanishMexican || locales.Spanish,
-          },
-      );
+    switch (cmd) {
+      case 'buscar':
+        let term;
+        let type = args.shift();
+        type = _.isString(type) ? type.toLowerCase() : null;
+        type = itemTypeMap[type];
+        term = args.join(' ');
 
-      const requests = response.Response.results.results.map(function(result) {
-        const displayProperties = _.get(result, 'displayProperties');
-        return traveler.getDestinyEntityDefinition(
-            TypeDefinition.DestinyInventoryItemDefinition, result.hash,
-        ).then(function(response) {
-          return {
-            display: displayProperties,
-            data: response.Response,
-            hash: result.hash,
-          };
-        });
-      });
+        if (!type || term.length === 0) {
+          const embed = new Discord.RichEmbed()
+              .setTitle('Ayuda')
+              .setColor(0x00AE86)
+              .setDescription('Para buscar debes poner algo como esto: !buscar [opciones] [busqueda]')
+              .setTimestamp()
+              .addField('Opciones', _.keys(itemTypeMap).join(', '), true);
 
-      let results = await Q.allSettled(requests);
-
-      results = _.compact(results.map(r => r.state === 'fulfilled' ? r.value : null));
-      const filtered = results.filter(r => r.data.itemType === type);
-
-      if (filtered.length === 0) {
-        commandNotFound(message);
-        return;
-      }
-
-      filtered.forEach(result => {
-        const display = result.display;
-        const data = result.data;
-        const hashedName = display.name.toLowerCase().split(/ +/g).join('-');
-        const dit = `${ditbase}${result.hash}-${hashedName}`;
-        const embed = new Discord.RichEmbed()
-            .setTitle(display.name)
-            .setColor(0x00AE86)
-            .setDescription(display.description)
-            .setTimestamp()
-            .setURL(dit);
-
-        if (data.screenshot) {
-          embed.setImage(`${bungiebase}${data.screenshot}`);
-        } else if (display.hasIcon) {
-          embed.setThumbnail(`${bungiebase}${display.icon}`);
+          message.channel.send({embed});
+          return;
         }
 
-        message.channel.send({embed});
-      });
+        logger.debug('term of search', term);
 
-      break;
+        const response = await traveler.searchDestinyEntities(
+            querystring.escape(term),
+            TypeDefinition.DestinyInventoryItemDefinition,
+            {
+              groups: [DestinyStatsGroupType.Weapons, DestinyStatsGroupType.UniqueWeapon],
+              lc: locales.SpanishMexican || locales.Spanish,
+            },
+        );
 
-    case 'ayuda':
-      commandNotFound(message, true);
-      return;
-    default:
-      commandNotFound(message);
-      return;
-  }
+        const requests = response.Response.results.results.map(function(result) {
+          const displayProperties = _.get(result, 'displayProperties');
+          return traveler.getDestinyEntityDefinition(
+              TypeDefinition.DestinyInventoryItemDefinition, result.hash,
+          ).then(function(response) {
+            return {
+              display: displayProperties,
+              data: response.Response,
+              hash: result.hash,
+            };
+          });
+        });
 
+        let results = await Q.allSettled(requests);
+
+        results = _.compact(results.map(r => r.state === 'fulfilled' ? r.value : null));
+        const filtered = results.filter(r => r.data.itemType === type);
+
+        if (filtered.length === 0) {
+          commandNotFound(message);
+          return;
+        }
+
+        filtered.forEach(result => {
+          const display = result.display;
+          const data = result.data;
+          const hashedName = display.name.toLowerCase().split(/ +/g).join('-');
+          const dit = `${ditbase}${result.hash}-${hashedName}`;
+          const embed = new Discord.RichEmbed()
+              .setTitle(display.name)
+              .setColor(0x00AE86)
+              .setDescription(display.description)
+              .setTimestamp()
+              .setURL(dit);
+
+          if (data.screenshot) {
+            embed.setImage(`${bungiebase}${data.screenshot}`);
+          } else if (display.hasIcon) {
+            embed.setThumbnail(`${bungiebase}${display.icon}`);
+          }
+
+          message.channel.send({embed});
+        });
+
+        break;
+
+      case 'ayuda':
+        commandNotFound(message, true);
+        return;
+      default:
+        commandNotFound(message);
+        return;
+    }
+
+  });
+
+  logger.info('bot started');
+
+  bot.login(token);
+};
+
+rp({
+  method: 'GET',
+  uri: 'https://www.bungie.net/Platform/GetAvailableLocales',
+  headers: {
+    'X-API-Key': process.env.BUNGIE_API_KEY,
+  },
+  json: true,
+}).then(onLocaleResponse).catch(function(error) {
+  logger.error(error, 'unable to read locales');
 });
-
-logger.info('bot started');
-
-bot.login(token);
-
-//
